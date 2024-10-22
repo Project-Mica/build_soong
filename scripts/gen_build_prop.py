4507d6e0365de97b22eaa14b2a16f8be7a5990b1
basamaryan <basam.aryan@gmail.com>
2024-10-22 12:08:07 -0400

    gen_build_prop: Stop overriding ro.build.display.id for non-user builds
    Change-Id: Ifc5b5fb23963a984f971ad0fda8c4cc3aec771ab

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -130,6 +130,7 @@ def parse_args():
     config["BuildThumbprint"] = args.build_thumbprint_file.read().strip()
 
   config["MicaDevice"] = config["DeviceName"]
+  config["MicaDesc"] = config["BuildDesc"]
 
   override_config(config)
 
@@ -222,7 +223,7 @@ def generate_build_info(args):
       print(f"ro.build.display.id?={config['BuildId']} {config['BuildKeys']}")
   else:
     # Non-user builds should show detailed build information (See build desc above)
-    print(f"ro.build.display.id?={config['BuildDesc']}")
+    print(f"ro.build.display.id?={config['MicaDesc']}")
   print(f"ro.build.version.incremental={config['BuildNumber']}")
   print(f"ro.build.version.sdk={config['Platform_sdk_version']}")
   print(f"ro.build.version.sdk_full={config['Platform_sdk_version_full']}")
